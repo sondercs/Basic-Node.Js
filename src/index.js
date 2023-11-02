c4827bf9f65848e2b5d75a2e6acc618b0174048f
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-11-02 22:51:18 +0000

    Adicionar o Nodemon na aplicação

--- src/index.js
+++ src/index.js
@@ -4,8 +4,31 @@ const res = require("express/lib/response");
 
 const app = express();
 
-app.get("/", (res, req) => {
-  return req.json({ message: "Hello Word" });
+app.get("/courses", (request, response) => {
+  return response.json(["Curso V1", "Curso V2", "Curso V3"]);
+  // localhost:3333/courses
+});
+
+app.post("/courses", (request, response) => {
+  return response.json(["Curso V4", "Curso V5", "Curso V6"]);
+  // localhost:3333/courses
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Curso V7", "Curso V8", "Curso V9"]);
+  // localhost:3333/courses/:id
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso V10", "Curso V11", "Curso V12"]);
+  // localhost:3333/courses/:id
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso V4", "Curso V5"]);
+  [
+    // localhost:3333/courses/:id
+  ];
 });
 
 app.listen(3333);
